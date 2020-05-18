b5003e2274b7c7c96fe4df5f6b2edc70f929e5c2
Anton Petrov <anton.petrov@innovecs.com>
2020-05-18 11:59:54 +0300

    Initial Commit

---
+++ MVVMCore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVMCore
+//
+//  Created by Petrov Anton on 5/18/20.
+//  Copyright © 2020 APM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
